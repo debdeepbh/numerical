2dfdaa212b6968c68baa998f18ef718ec87c6987
Debdeep <debdeep777@gmail.com>
2018-04-29 23:38:41 -0400

    compare error in different norms

--- wavelet/testvec-2.m
+++ wavelet/testvec-2.m
@@ -1,8 +1,11 @@
 % defining the testvec
 testvec = zeros(1,512);
 for j = 32:95
-       testvec(j+1) = (j - 256)*exp(-(j-256)^2/512);
+	testvec(j+1) =	1;
 end
-
-
-
+for j = 132:259
+	testvec(j+1) =	2;
+end
+for j = 325:415
+	testvec(j+1) =	4;
+end
