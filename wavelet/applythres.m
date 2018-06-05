67a14aa4f68e8e412995663255b77963af6feabd
Debdeep <debdeep777@gmail.com>
2018-06-04 18:06:23 -1000

    thresholding

---
+++ wavelet/applythres.m
@@ -0,0 +1,41 @@
+% apply threshold on the p-th stage transform
+% thr should be a vector of length p+1
+function w = applythres(wo, p, thr)
+
+% don't want to replace the original
+w = wo;
+N = length(w);
+
+% check
+if length(thr) ~= p+1
+	disp('threshold vector length incorrect');
+end
+
+
+% staring index
+starting = 1;
+len = N/2;
+for k=1:p
+	ending = starting+len-1;
+	for l=starting:ending
+		if( w(l) < thr(k))
+			w(l) = 0;
+		end
+	end
+	% for the next loop
+	len = len/2;
+	starting = ending + 1;
+end
+
+% for the coarsest level Phi_{-p,k}
+for l=starting:N
+	if( w(l) < thr(p+1))
+		w(l) = 0;
+	end
+end
+
+
+
+
+
+
