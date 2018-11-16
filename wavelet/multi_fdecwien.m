1148c7f871a83943cb5d0ad31388711a57a94868
Debdeep <6978309+debdeepbh@users.noreply.github.com>
2018-11-15 19:51:52 -0500

    multi-antenna model

---
+++ wavelet/multi_fdecwien.m
@@ -0,0 +1,22 @@
+% Wiener deconvolution in the frequency domain
+% assume that the fsig and fimp are of the same length
+function [fw, mult] = multi_fdecwien(fsig, fimp, noise, fori, scaling)
+
+
+N = length(fsig);
+
+
+	% definition
+	hsq = abs(fimp).^2;
+
+	% construct the multiplier
+	if (length(noise)==1)	% noise is actually the  sd
+		sigmasq = N*noise^2;
+	else			% it is the raw noise
+		sigmasq = abs(fft(noise)).^2;
+	end
+	fw = fsig .* conj(fimp) ./( hsq + scaling*(sigmasq)./(abs(fori).^2) );
+	% we need to output multi as well, for wienforwd.m
+	mult = hsq ./( hsq + scaling*(sigmasq)./(abs(fori).^2) );
+% multiply by the scaling and take inverse fourier transform
+%fw = wfft.*mult;
