44c8c6d97e0845ca43d7a0ad43156ec40ec48242
Debdeep <debdeep777@gmail.com>
2020-07-20 19:10:01 -0500

    functional damping

--- msh-octave/pairwise_forces.m
+++ msh-octave/pairwise_forces.m
@@ -1,9 +1,8 @@
-function [total_contact_force, total_friction_force] = pairwise_forces(contact_NbdArr, CurrPos_center, CurrPos_neighbor, velocity_center, velocity_neighbor, Vol_neighbor, contact_radius, normal_stiffness, friction_coefficient)
+function [total_contact_force, total_friction_force, total_damping_force] = pairwise_forces(CurrPos_center, CurrPos_neighbor, velocity_center, velocity_neighbor, Vol_neighbor, contact_radius, normal_stiffness, friction_coefficient, damping_ratio, rho)
 
 % Computes the pairwise forces: contact force, friction force on the central body due to the neighboring body
 % 
 % Input:
-%	contact_NbdArr: matrix containing the indices within the neighboring body that are contact-radius close to the nodes of the central body
 %	CurrPos_center:  Current position vector of central body, nx2
 %	CurrPos_neighbor:Current position vector of neighboring body, mx2
 %	Vol_neighbor: Volume associated with the nodes of the neighboring body, mx1
@@ -14,6 +13,12 @@ function [total_contact_force, total_friction_force] = pairwise_forces(contact_N
 %	total_contact_force: Total contact force being applied on the nodes of the central body due to the neighboring body, nx2
 %	direction_unit_1: first component of direction vector from the center to neighbor, nxl, l=#neighbors
 %	direction_unit_2: second component of direction vector from the center to neighbor, nxl, l=#neighbors
+
+
+%	contact_NbdArr: matrix containing the indices within the neighboring body that are contact-radius close to the nodes of the central body
+% nodes in the neighboring body that are contact_radius-close to the nodes in the central body
+%% edit this function to output the difference between the points as well!!
+[contact_NbdArr] = gen_NbdArr_varlength(CurrPos_center, CurrPos_neighbor, contact_radius, 0);
 
 CurrPos_center_1 = CurrPos_center(:,1);
 CurrPos_center_2 = CurrPos_center(:,2);
@@ -62,36 +67,76 @@ vel_neighbor_2 = velocity_neighbor(:,2);
 % this is actually the contact force density
 contact_force_norm = sqrt(nbd_contact_force_1.^2 + nbd_contact_force_2.^2);
 
-% relative velocity
-vel_direction_1 = (vel_neighbor_1(contact_NbdArr + ~contact_NbdArr) - vel_center_1);
-vel_direction_2 = (vel_neighbor_2(contact_NbdArr + ~contact_NbdArr) - vel_center_2);
-%vel_direction_1 = (vel_neighbor_1(contact_NbdArr + ~contact_NbdArr) - vel_center_1) .* mask;
-%vel_direction_2 = (vel_neighbor_2(contact_NbdArr + ~contact_NbdArr) - vel_center_2) .* mask;
+% relative velocity of the center, i.e. the velocity of the nodes of the central body observed from static neighboring nodes
+rel_vc_1 = -(vel_neighbor_1(contact_NbdArr + ~contact_NbdArr) - vel_center_1);
+rel_vc_2 = -(vel_neighbor_2(contact_NbdArr + ~contact_NbdArr) - vel_center_2);
 
-vel_direction_norm = sqrt(vel_direction_1.^2  + vel_direction_2.^2) .* mask;
+rel_vc_norm = sqrt(rel_vc_1.^2 + rel_vc_2.^2);
+% unit relative velocity of the center
+rel_vc_unit_1 = rel_vc_1 ./ (rel_vc_norm + ~rel_vc_norm) .* mask;
+rel_vc_unit_2 = rel_vc_2 ./ (rel_vc_norm + ~rel_vc_norm) .* mask;
 
-%  in the direction from center to neighbor
-vel_direction_unit_1 = vel_direction_1 ./ (vel_direction_norm + ~vel_direction_norm) .* mask;
-vel_direction_unit_2 = vel_direction_2 ./ (vel_direction_norm + ~vel_direction_norm) .* mask;
+% (v.e), projection of unit relative velocity of center onto the direction unit vector (from center to neighbor)
+rel_vc_normal_projection = rel_vc_unit_1 .* nbd_direction_unit_1 + rel_vc_unit_2 .* nbd_direction_unit_2;
 
-% projection of unit velocity onto the direction unit vector
-vel_normal_projection = vel_direction_unit_1 .* nbd_direction_unit_1 + vel_direction_unit_2 .* nbd_direction_unit_2; 
+% v - (v.e)e, relative velocity of center in the tangential direction
+rel_vc_tangential_1 = (rel_vc_unit_1 - rel_vc_normal_projection .* nbd_direction_unit_1) .* mask;
+rel_vc_tangential_2 = (rel_vc_unit_2 - rel_vc_normal_projection .* nbd_direction_unit_2) .* mask;
 
-% v - (v.e)e
-vel_tangential_1 = (vel_direction_1 - vel_normal_projection .* nbd_direction_unit_1) .* mask; 
-vel_tangential_2 = (vel_direction_2 - vel_normal_projection .* nbd_direction_unit_2) .* mask;
+% |v - (v.e)e|
+rel_vc_tangential_norm = sqrt(rel_vc_tangential_1.^2 + rel_vc_tangential_2.^2);
 
-vel_tangential_norm = sqrt(vel_tangential_1.^2 + vel_tangential_2.^2);
-
-vel_tangential_unit_1 = vel_tangential_1 ./ (vel_tangential_norm + ~vel_tangential_norm) .* mask;
-vel_tangential_unit_2 = vel_tangential_2 ./ (vel_tangential_norm + ~vel_tangential_norm) .* mask;
+% normalized (v - (v.e)e), unit vector normal to contact vector
+rel_vc_tangential_unit_1 = rel_vc_tangential_1 ./ (rel_vc_tangential_norm + ~rel_vc_tangential_norm) .* mask;
+rel_vc_tangential_unit_2 = rel_vc_tangential_2 ./ (rel_vc_tangential_norm + ~rel_vc_tangential_norm) .* mask;
 
 % % tangential friction force acts in the opposite direction of velocity
-friction_force_1 = friction_coefficient .* contact_force_norm .* vel_tangential_unit_1;
-friction_force_2 = friction_coefficient .* contact_force_norm .* vel_tangential_unit_2;
-%friction_force_1 = friction_coefficient .* contact_force_norm .* vel_tangential_1;
-%friction_force_2 = friction_coefficient .* contact_force_norm .* vel_tangential_2;
+
+% % original expression (31) of Peridem.pdf, friction force acts normal to contact dir
+% friction_force_1 = -friction_coefficient .* contact_force_norm .* rel_vc_tangential_1;
+% friction_force_2 = -friction_coefficient .* contact_force_norm .* rel_vc_tangential_2;
+
+% modification, with normalized unit vector
+friction_force_1 = -friction_coefficient .* contact_force_norm .* rel_vc_tangential_unit_1;
+friction_force_2 = -friction_coefficient .* contact_force_norm .* rel_vc_tangential_unit_2;
+
+
+% % modification, direction of friction is opposite to the relative velocity
+% % magnitude of friction is the norm of normal contact force
+%friction_force_1 = -friction_coefficient .* contact_force_norm .* rel_vc_unit_1; 
+%friction_force_2 = -friction_coefficient .* contact_force_norm .* rel_vc_unit_2; 
+
+% % magnitude: projection of contact force perpendicular to relative velocity
+% % v_perp = e - (e.v)v
+% v_perp_1 = (nbd_direction_unit_1 - rel_vc_normal_projection .* rel_vc_unit_1) .* mask; 
+% v_perp_2 = (nbd_direction_unit_2 - rel_vc_normal_projection .* rel_vc_unit_2) .* mask; 
+% 
+% v_perp_norm = sqrt(v_perp_1.^2 + v_perp_2.^2);
+% % unit vector perpendicular to velocity, outward of the central body
+% v_perp_unit_1 = v_perp_1 ./ (v_perp_norm + ~v_perp_norm) .* mask;
+% v_perp_unit_2 = v_perp_2 ./ (v_perp_norm + ~v_perp_norm) .* mask;
+% 
+% contact_force_projection = (-nbd_contact_force_1) .* v_perp_unit_1 + (-nbd_contact_force_2) .* v_perp_unit_2;
+
+% friction_force_1 = - friction_coefficient .* contact_force_projection .* rel_vc_unit_1;
+% friction_force_2 = - friction_coefficient .* contact_force_projection .* rel_vc_unit_2;
 
 total_friction_force = [sum(friction_force_1, 2), sum(friction_force_2,2)];  
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                            Damping force                          %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+rel_vc_full_proj = rel_vc_1 .* nbd_direction_unit_1 + rel_vc_2 .* nbd_direction_unit_2;
+
+% % relative velocity of the neighbor
+% (v.e)e where v is the velocity of the center
+rel_v_neighbor_component_1 = rel_vc_full_proj .* nbd_direction_unit_1;
+rel_v_neighbor_component_2 = rel_vc_full_proj .* nbd_direction_unit_2;
+
+damping_coefficient = 2 * damping_ratio .* sqrt(normal_stiffness * rho) .* sqrt(cnbd_Vol_neighbor);
+
+damping_force_1 = - damping_coefficient .* rel_v_neighbor_component_1 .* mask;
+damping_force_2 = - damping_coefficient .* rel_v_neighbor_component_2 .* mask;
+
+total_damping_force = [sum(damping_force_1, 2), sum(damping_force_2,2)];  
